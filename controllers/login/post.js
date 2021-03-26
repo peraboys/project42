7999497a6508de27186d5c5311c59cc77581b1ce
sinanergezer <34683867+sinanergezer@users.noreply.github.com>
2021-03-27 01:47:22 +0300

    title fixed

--- controllers/login/post.js
+++ controllers/login/post.js
@@ -1,6 +1,17 @@
+const body=[];
+var isim="";
 module.exports = (req, res) => {
-    console.log(req.body.name);
+  req.on('data',(chunk)=>{
+    body.push(chunk);
+    console.log(chunk);
+  } );
+  req.on('end',()=>{
+    const parsed=Buffer.concat(body).toString();
+    isim =parsed.split('=')[1].split('&')[0];
+    //console.log(name);
+  });
+    //console.log(req.body.name);
     return res.render('login',{
-    title:req.body.name
+    title: isim
     })
   }
