e73676fdfc14d3527e074a110c48b76a1463ef0a
sinanergezer <34683867+sinanergezer@users.noreply.github.com>
2021-03-27 19:06:38 +0300

    title update problem fixed

--- controllers/login/post.js
+++ controllers/login/post.js
@@ -1,17 +1,18 @@
-const body=[];
+
 var isim="";
 module.exports = (req, res) => {
+  const body=[];
   req.on('data',(chunk)=>{
     body.push(chunk);
-    console.log(chunk);
   } );
   req.on('end',()=>{
     const parsed=Buffer.concat(body).toString();
     isim =parsed.split('=')[1].split('&')[0];
-    //console.log(name);
+    console.log(isim);
+    return res.render('login',{
+      title: isim
+      })
   });
     //console.log(req.body.name);
-    return res.render('login',{
-    title: isim
-    })
+    
   }
