6daacf6a7d12f765ff09a4244ff699cfbe636df7
sinanergezer <34683867+sinanergezer@users.noreply.github.com>
2021-04-17 01:47:21 +0200

    sendgrid key removed

--- controllers/resetPassword/post.js
+++ controllers/resetPassword/post.js
@@ -1,6 +1,5 @@
 
 const sgMail = require('@sendgrid/mail')
-//sgMail.setApiKey("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");
 module.exports=(req,res)=>{
     const msg = {
         to: req.body.email, // Change to your recipient
