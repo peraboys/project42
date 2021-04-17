181c31adb8c00324805b3e6817b5efe875b37fde
sinanergezer <34683867+sinanergezer@users.noreply.github.com>
2021-04-18 01:42:06 +0200

    removed console.log for api key

--- app.js
+++ app.js
@@ -57,7 +57,6 @@ app.use((req,res,next)=>{
 mongoose.connect(connectionString,{ useFindAndModify: false })
 .then(()=>{
 console.log("connected to mongodb");
-console.log(process.env.SENDGRID_API_KEY);
 app.listen(3000);
 })
 .catch(err=>{
