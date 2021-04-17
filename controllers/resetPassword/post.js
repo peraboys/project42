fb1ea23b332b2c3408f1fe9da6b818148c96a31b
sinanergezer <34683867+sinanergezer@users.noreply.github.com>
2021-04-18 01:38:41 +0200

    changed sender

--- controllers/resetPassword/post.js
+++ controllers/resetPassword/post.js
@@ -4,7 +4,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 module.exports=(req,res)=>{
     const msg = {
         to: req.body.email, // Change to your recipient
-        from: 'sinanergezer@gmail.com', // Change to your verified sender
+        from: 'atalaykorkut@hotmail.com', // Change to your verified sender
         subject: 'Reset youtr password',
         text: 'Reset your password'
         
