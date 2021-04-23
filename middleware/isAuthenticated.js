4ecfaeacfb67398bd8d7c1f51e3e5ece7d09fb10
Atalay Korkut <atalaykorkut@hotmail.com>
2021-04-24 00:36:08 +0200

    intro to alert

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -1,6 +1,14 @@
+//const popup = require('node-popup');
+var JSAlert = require("js-alert");
+//const popup = require('node-popup/dist/cjs.js');
 module.exports=(req,res,next)=>{
     if(!req.session.isAuth){
-      return res.redirect('/login');
-    }
+     // import {alert} from 'node-popup';
+  //popup.alert('aaa');
+  JSAlert.alert('aaa');
+      return res.render('login',{
+      denied: true
+    })
     next();
+}
 }
