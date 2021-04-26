b51e45a1d48675f3711286a8106b585abebbf9be
sinanergezer <34683867+sinanergezer@users.noreply.github.com>
2021-04-26 23:36:27 +0200

    changed navbar style

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -1,14 +1,12 @@
 //const popup = require('node-popup');
-var JSAlert = require("js-alert");
+//var JSAlert = require("js-alert");
 //const popup = require('node-popup/dist/cjs.js');
 module.exports=(req,res,next)=>{
     if(!req.session.isAuth){
-     // import {alert} from 'node-popup';
-  //popup.alert('aaa');
-  JSAlert.alert('aaa');
       return res.render('login',{
       denied: true
     })
-    next();
+  
 }
+next();
 }
